bc2825e63cede5ffeb93b116b8e1636716d94b37
sossw1 <sossw1@gmail.com>
2021-02-02 18:31:13 -0500

    Seed data

--- team.sql
+++ team.sql
@@ -25,3 +25,39 @@ CREATE TABLE employees (
     manager_id INT,
     PRIMARY KEY (id)
 );
+
+INSERT INTO departments (name)
+VALUES ("Accounting");
+
+INSERT INTO departments (name)
+VALUES ("Payroll");
+
+INSERT INTO departments (name)
+VALUES ("Marketing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Accountant", 55000, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Jr. Accountant", 45000, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Payroll Specialist", 45000, 2);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Payroll Manager", 60000, 2);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Graphic Designer", 50000, 3);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Project Manager", 70000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES("Jenny", "Maven", 4, NULL);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES("Martin", "Roth", 6, NULL);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES("Jerry", "Smith", 1, NULL);
