19265e60d840d9c9c9e474d093d4c639e49142f3
sossw1 <sossw1@gmail.com>
2021-02-02 21:37:41 -0500

    Updated seeds

--- team.sql
+++ team.sql
@@ -36,10 +36,10 @@ INSERT INTO departments (name)
 VALUES ("Marketing");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Accountant", 55000, 1);
+VALUES ("Jr. Accountant", 45000, 1);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Jr. Accountant", 45000, 1);
+VALUES ("Accountant", 55000, 1);
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Payroll Specialist", 45000, 2);
@@ -52,6 +52,9 @@ VALUES ("Graphic Designer", 50000, 3);
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Project Manager", 70000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES("Barry", "Downs", 2, NULL);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES("Jenny", "Maven", 4, NULL);
